483004314e1cd945ab107eb0b4d012915e9c99f2
Cheng-DX <2337040896@qq.com>
2022-10-19 23:08:23 +0800

    feat: remove maybe-ref stuff in shared package

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -2,8 +2,3 @@ export type { MaybeCallable } from './maybeCallable'
 export { resolveCallable } from './maybeCallable'
 
 export { createArray } from './createArray'
-
-export type { MaybeComputedRef, MaybeRef } from './maybeRef'
-
-export { resolveRef } from './resolveRef'
-export { resolveUnref } from './resolveUnref'
--- packages/shared/src/maybeRef/index.ts
+++
@@ -1,4 +0,0 @@
-import type { ComputedRef, Ref } from 'vue'
-
-export type MaybeRef<T> = Ref<T> | T
-export type MaybeComputedRef<T> = (() => T) | MaybeRef<T> | ComputedRef<T>
