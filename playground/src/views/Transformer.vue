d893b58b6fde9c94f8a1f3963e9626dd04dda93a
Cheng-DX <2337040896@qq.com>
2022-10-31 21:43:28 +0800

    test: transformer test

---
+++ playground/src/views/Transformer.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import type { Format } from '@chengdx/shared'
+import { format } from '@chengdx/shared'
+
+const input = ref('')
+const type = ref<Format>('camel')
+const output = computed(() => format(input.value, type.value).result)
+</script>
+
+<template>
+  <n-space>
+    <n-input v-model:value="input" style="width: 200px" />
+    <n-select
+      v-model:value="type"
+      style="width: 100px"
+      :options="[
+        { label: 'camel', value: 'camel' },
+        { label: 'kebab', value: 'kebab' },
+        { label: 'snake', value: 'snake' },
+        { label: 'pascal', value: 'pascal' },
+      ]"
+    />
+  </n-space>
+  {{ output }}
+</template>
+
+<style scoped>
+
+</style>
--- playground/src/views/index.vue
+++ playground/src/views/index.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import UseSelection from './UseSelection.vue'
+import Transformer from './Transformer.vue'
 </script>
 
 <template>
   <UseSelection />
+  <Transformer />
 </template>
 
 <style scoped>
