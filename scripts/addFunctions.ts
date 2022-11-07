ab3886b6caa4fa37f24ea821f30a26d35282b9c2
Cheng-DX <2337040896@qq.com>
2022-11-07 17:20:36 +0800

    chore: use fs/promises

--- scripts/addFunctions.ts
+++ scripts/addFunctions.ts
@@ -1,4 +1,4 @@
-import fs from 'fs'
+import fs from 'fs/promises'
 import { getExportsRuntime } from 'pkg-exports'
 import { formatNaming, replaceSubstring } from '@chengdx/shared'
 import consola from 'consola'
@@ -9,11 +9,18 @@ const packages = getPackages(pkg => pkg !== '@chengdx/default-export-resolver')
 packages.forEach(async (pkg) => {
   const exports: string[] = await getExportsRuntime(pkg)
   const url = `packages/${pkg.split('/').pop()}`
-  const content = fs.readFileSync(`${url}/README.md`).toString()
-  const dirs = fs.readdirSync(`${url}/src`).map((dir) => {
-    const isDirectory = fs.statSync(`${url}/src/${dir}`).isDirectory()
-    return isDirectory ? formatNaming(dir, 'kebab').result : ''
-  }).filter(dir => dir)
+  const content = (await fs.readFile(`${url}/README.md`)).toString()
+  const dirs = await fs.readdir(`${url}/src`)
+  const fotmattedDirs = await Promise.all(
+    dirs
+      .map(async (dir) => {
+        const isDirectory = (await fs.stat(`${url}/src/${dir}`)).isDirectory()
+        return isDirectory ? formatNaming(dir, 'kebab').result : ''
+      })
+      .filter(async (dir) => {
+        return await dir
+      }),
+  )
 
   const startTag = '<!-- FUNCTIONS START -->\n'
   const start = content.indexOf(startTag) + startTag.length
@@ -22,11 +29,11 @@ packages.forEach(async (pkg) => {
 
   const path = (func: string) => {
     const kebab = formatNaming(func, 'kebab').result
-    return dirs.includes(kebab)
+    return fotmattedDirs.includes(kebab)
       ? `- [${func}](src/${kebab}/index.md)`
       : `- ${func}`
   }
   const functions = `${exports.map(func => path(func)).join('\n')}\n`
-  fs.writeFileSync(`${url}/README.md`, replaceSubstring(content, start, end, functions))
+  await fs.writeFile(`${url}/README.md`, replaceSubstring(content, start, end, functions))
   consola.success(`Resolved ${pkg}`)
 })
