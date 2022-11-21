05539545638daf72b128890c0266f47c87c4099b
Cheng-DX <2337040896@qq.com>
2022-11-21 16:46:15 +0800

    feat: update version scripts

--- scripts/getPackages.ts
+++ scripts/getPackages.ts
@@ -1,6 +1,27 @@
+import fs from 'fs'
+import consola from 'consola'
 import { dependencies } from '../package.json'
 
 export type Package = keyof typeof dependencies
-export function getPackages(filter: (pkg: Package) => boolean) {
-  return (Object.keys(dependencies) as Package[]).filter(filter)
+export function getPackages(filter?: (pkg: Package) => boolean) {
+  return (Object.keys(dependencies) as Package[]).filter(filter ?? (() => true))
+}
+
+export function getOrCreateFile(
+  filepath: string,
+  filter?: (pkg: Package) => boolean,
+) {
+  return getPackages(filter).map((pkg) => {
+    const resolvedPath = filepath.startsWith('/') ? filepath : `/${filepath}`
+    const fullPath = `packages/${pkg.split('/').pop()}${resolvedPath}`
+
+    if (!fs.existsSync(fullPath)) {
+      fs.mkdirSync(fullPath, { recursive: true })
+      consola.info(`Created ${filepath} for ${pkg}`)
+    }
+    return {
+      pkg,
+      path: fullPath,
+    }
+  })
 }
---
+++ scripts/updateVersion.ts
@@ -0,0 +1,37 @@
+import fs from 'fs'
+import consola from 'consola'
+import parseArgs from 'minimist'
+import { getOrCreateFile } from './getPackages'
+
+type UpdateType = 'major' | 'minor' | 'patch'
+
+getOrCreateFile('package.json').forEach(({ pkg, path }) => {
+  const content = JSON.parse(fs.readFileSync(path, 'utf-8'))
+  const { version } = content
+  const splited = (version as string).split('.').map(v => parseInt(v))
+  if (splited.length !== 3)
+    consola.error(`Invalid version ${version} for ${pkg}`)
+
+  const [major, minor, patch] = splited
+  const { _: [type = 'patch'] } = parseArgs(process.argv.slice(2))
+  let newVersion
+
+  switch (type as UpdateType) {
+    case 'major':
+      newVersion = `${major + 1}.0.0`
+      break
+    case 'minor':
+      newVersion = `${major}.${minor + 1}.0`
+      break
+    case 'patch':
+      newVersion = `${major}.${minor}.${patch + 1}`
+      break
+    default:
+      consola.error(`Invalid type ${type}`)
+  }
+
+  content.version = newVersion
+  fs.writeFileSync(path, JSON.stringify(content, null, 2))
+  consola.info(`Updating ${pkg} from ${version} to ${newVersion}`)
+})
+
