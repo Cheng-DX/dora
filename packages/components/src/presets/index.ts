dd618657e42b8648370dcd1c5f208d5cbd9dab78
Cheng-DX <2337040896@qq.com>
2022-11-27 20:51:41 +0800

    fix:  wrong action when the path is a file

--- packages/components/src/presets/index.ts
+++ packages/components/src/presets/index.ts
@@ -2,5 +2,6 @@ export default {
   '@chengdx/components': [
     'PostContainer',
     'PostItem',
+    'presets',
   ],
 }
--- scripts/getPackages.ts
+++ scripts/getPackages.ts
@@ -17,18 +17,21 @@ export function getFile(
   const { filter, createOnNotExisted = true } = options || {}
   return getPackages(filter).map((pkg) => {
     let existed = true
-    const resolvedPath = filepath.startsWith('/') ? filepath : `/${filepath}`
-    const fullPath = `packages/${pkg.split('/').pop()}${resolvedPath}`
+    const resolvedPath = filepath.startsWith('/') ? filepath.slice(1) : filepath
+    const hasDir = resolvedPath.split('/').length > 1
+    const dirName = `packages/${pkg.split('/').pop()}/${resolvedPath.split('/').slice(0, -1).join('/')}`
+    const fileName = resolvedPath.split('/').pop()
+    const fullPath = `${dirName}/${fileName}`
 
     if (!fs.existsSync(fullPath)) {
-      if (!createOnNotExisted) {
-        existed = false
-      }
-      else {
-        // FIXME: wrong reaction when the path is a file
-        fs.mkdirSync(fullPath, { recursive: true })
+      if (!createOnNotExisted) { existed = false }
+
+      else if (hasDir) {
+        fs.mkdirSync(dirName, { recursive: true })
         consola.info(`Created ${filepath} for ${pkg}`)
       }
+
+      fs.writeFileSync(fullPath, '')
     }
     return {
       pkg,
