a4f5f2effa5b955882cb539d6c9a2e6890cdf6fe
Cheng-DX <2337040896@qq.com>
2022-12-08 18:26:43 +0800

    release: v0.10.1

--- packages/naive-ui/src/render-icon/index.ts
+++ packages/naive-ui/src/render-icon/index.ts
@@ -3,6 +3,5 @@ import type { Component } from 'vue'
 import { h } from 'vue'
 
 export function renderIcon(icon: Component, attrs: Record<string, unknown> = {}) {
-  // @ts-expect-error WTF
   return () => h(NIcon, attrs, { default: () => h(icon) })
 }
--- packages/shared/src/presets/index.ts
+++ packages/shared/src/presets/index.ts
@@ -14,5 +14,7 @@ export default {
     'replaceSubstring',
     'resolveArray',
     'resolveCallable',
+    'resolveMatch',
+    'resolveMatches',
   ],
 }
