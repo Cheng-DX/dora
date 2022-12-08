d28559cb65301ec05cc496a72d8b774d8d9e90b5
Cheng-DX <2337040896@qq.com>
2022-12-08 18:28:23 +0800

    fix: wrong ts-error

--- packages/naive-ui/src/render-icon/index.ts
+++ packages/naive-ui/src/render-icon/index.ts
@@ -2,6 +2,6 @@ import { NIcon } from 'naive-ui'
 import type { Component } from 'vue'
 import { h } from 'vue'
 
-export function renderIcon(icon: Component, attrs: Record<string, unknown> = {}) {
-  return () => h(NIcon, attrs, { default: () => h(icon) })
+export function renderIcon(icon: Component, attrs: Record<string, any>) {
+  return () => h(NIcon, { ...attrs }, { default: () => h(icon) })
 }
