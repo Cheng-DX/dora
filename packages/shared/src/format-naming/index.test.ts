a518fc11709a405431c120bdef13d2afb5628b1b
Cheng-DX <2337040896@qq.com>
2022-12-08 18:19:23 +0800

    style: lint

--- packages/shared/src/format-naming/index.test.ts
+++ packages/shared/src/format-naming/index.test.ts
@@ -4,5 +4,8 @@ import { formatNaming } from '.'
 describe('transformVariableNaming', () => {
   it('should matched', () => {
     expect(formatNaming('foo-file', 'camel').result).toMatchInlineSnapshot('"fooFile"')
+    expect(formatNaming('foo-file', 'pascal').result).toMatchInlineSnapshot('"FooFile"')
+    expect(formatNaming('foo-file', 'kebab').result).toMatchInlineSnapshot('"foo-file"')
+    expect(formatNaming('foo-file', 'snake').result).toMatchInlineSnapshot('"foo_file"')
   })
 })
