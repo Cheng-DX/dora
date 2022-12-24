a5480864bc3a7da556cbe231bbbcc4747a57a9da
Cheng-DX <2337040896@qq.com>
2022-12-24 17:04:06 +0800

    feat(naive-ui): add renderlabel in `use-selection`

--- packages/naive-ui/src/use-selection/index.ts
+++ packages/naive-ui/src/use-selection/index.ts
@@ -1,20 +1,21 @@
+import type { VNodeChild } from 'vue'
 import { computed, ref, unref } from 'vue'
 import type { MaybeRef } from '@chengdx/maybe-ref'
+import type { SelectOption } from 'naive-ui'
 
 export function useSelection<T extends Object>(
   _options: MaybeRef<{
     label: string
     value: T
   }[]>,
+  /**
+   * render object to vNode
+   */
+  renderer?: (selected: T) => VNodeChild,
 ) {
   const value = ref<string>()
   const selected = computed<T>(() => {
-    try {
-      return JSON.parse(value.value || '{}')
-    }
-    catch (e) {
-      return {}
-    }
+    return value2Object(value.value)
   })
   const options = computed(() => {
     try {
@@ -30,10 +31,27 @@ export function useSelection<T extends Object>(
       return []
     }
   })
+  const renderLabel = renderer
+    ? (option: SelectOption) => {
+        const value = option.value as string
+        const t = value2Object<T>(value)
+        return renderer(t)
+      }
+    : undefined
 
   return {
     selected,
     value,
     options,
+    renderLabel,
+  }
+}
+
+function value2Object<T>(value?: string) {
+  try {
+    return JSON.parse(value || '{}') as T
+  }
+  catch (e) {
+    return {} as T
   }
 }
