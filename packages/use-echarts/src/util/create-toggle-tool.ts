ce25d901223cb2d346d2077a38d4b0fb3ce4e781
Cheng-DX <2337040896@qq.com>
2022-12-28 22:52:52 +0800

    feat(use-echarts): refector createToggleTool

--- packages/use-echarts/src/util/create-toggle-tool.ts
+++ packages/use-echarts/src/util/create-toggle-tool.ts
@@ -1,32 +1,72 @@
 import type { MaybeCallable } from '@chengdx/shared'
-import { resolveCallable } from '@chengdx/shared'
+import { capitalize, resolveCallable } from '@chengdx/shared'
 import type { EChartsOption } from 'echarts'
-import type { Ref } from 'vue'
+import { computed, ref } from 'vue'
 
 export function createToggleTool(
-  v: Ref<boolean>,
-  name: string,
-  title: MaybeCallable<string>,
-  icon: MaybeCallable<string>,
-): EChartsOption {
-  // custom tool must start with my
-  // see https://echarts.apache.org/option.html#toolbox.feature
-  const resolvedName = name.startsWith('my')
-    ? name
-    : `my${name[0].toUpperCase()}${name.slice(1)}`
+  config: {
+    /**
+     * the function that will be called when the tool is clicked
+     * @param v current state
+     */
+    onClick: (v: boolean) => void
+    /**
+     * the name can be a string or a function that returns a string
+     * @see [maybeCallable](https://github.com/Cheng-DX/dora/blob/main/packages/shared/src/maybe-callable/index.md)
+     */
+    name: MaybeCallable<string, [boolean]>
+    /**
+     * the title can be a string or a function that returns a string
+     * @see [maybeCallable](https://github.com/Cheng-DX/dora/blob/main/packages/shared/src/maybe-callable/index.md)
+     */
+    title: MaybeCallable<string, [boolean]>
+    /**
+     * the icon can be a string or a function that returns a string
+     * @see [maybeCallable](https://github.com/Cheng-DX/dora/blob/main/packages/shared/src/maybe-callable/index.md)
+     */
+    icon: MaybeCallable<string, [boolean]>
+    /**
+     * @default false
+     */
+    initualValue?: boolean
+    /**
+     * Auto toogle the value when the tool is clicked
+     * @default false
+     */
+    autoToggle?: boolean
+  },
+) {
+  const { onClick, name, title, icon, initualValue = false, autoToggle = false } = config
+  const v = ref(initualValue)
 
-  return {
-    toolbox: {
-      feature: {
-        [resolvedName]: {
-          show: true,
-          title: resolveCallable(title, v.value),
-          icon: resolveCallable(icon, v.value),
-          onclick: () => {
-            v.value = !v.value
+  const tool = computed<EChartsOption>(() => {
+    // custom tool must start with my
+    // see https://echarts.apache.org/option.html#toolbox.feature
+    const resolvedName = resolveCallable(name, v.value)
+    const toolName = resolvedName.startsWith('my')
+      ? resolvedName
+      : `my${capitalize(resolvedName)}`
+
+    return {
+      toolbox: {
+        feature: {
+          [toolName]: {
+            show: true,
+            title: resolveCallable(title, v.value),
+            icon: resolveCallable(icon, v.value),
+            onclick: () => {
+              onClick(v.value)
+              if (autoToggle)
+                v.value = !v.value
+            },
           },
         },
       },
-    },
+    }
+  })
+
+  return {
+    tool,
+    v,
   }
 }
