0ae40c07123687f757aa11bc9a58d4c0233ac40f
Cheng-DX <2337040896@qq.com>
2022-12-29 00:17:05 +0800

    chore(shared): better type decleration for `createArray`

--- packages/shared/src/create-array/index.ts
+++ packages/shared/src/create-array/index.ts
@@ -1,3 +1,8 @@
-export function createArray<T>(length: number, mapFn?: (i: number) => T) {
+function createArray(length: number): undefined[]
+function createArray<T>(length: number, mapFn: (i: number) => T): T[]
+
+function createArray<T>(length: number, mapFn?: (i: number) => T) {
   return Array.from({ length }, (_, i) => mapFn?.(i))
 }
+
+export { createArray }
